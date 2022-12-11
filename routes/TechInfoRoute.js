22d6593948be82e3b749fd54f370f88b6f19a9fa
shahidx0x <shahidabdulla.aas@gmail.com>
2022-12-12 03:20:25 +0600

    controller updated

--- routes/TechInfoRoute.js
+++ routes/TechInfoRoute.js
@@ -2,11 +2,17 @@ const express = require("express");
 const {
   allTechInfo,
   addTechInfo,
+  findOneTech,
+  updateOne,
+  deleteOne,
 } = require("../controller/TechInfoController");
 
 const techinfo_router = express.Router();
 
 techinfo_router.get("/techinfo", allTechInfo);
 techinfo_router.post("/addtechinfo", addTechInfo);
+techinfo_router.get("/techinfo/:id", findOneTech);
+techinfo_router.patch("/techinfo/update/:id", updateOne);
+techinfo_router.delete("/techinfo/remove/:id", deleteOne);
 
 module.exports = techinfo_router;
